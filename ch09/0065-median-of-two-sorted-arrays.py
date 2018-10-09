768da65996cde4c66a0d8e5e8171b48fa1e4d0a0
zzy <zzy1120716@126.com>
2018-10-09 17:12:37 +0800

    ch09 finished

---
+++ ch09/0065-median-of-two-sorted-arrays.py
@@ -0,0 +1,115 @@
+"""
+65. 两个排序数组的中位数
+两个排序的数组A和B分别含有m和n个数，找到两个排序数组的中位数，
+要求时间复杂度应为O(log (m+n))。
+
+样例
+给出数组A = [1,2,3,4,5,6] B = [2,3,4,5]，中位数3.5
+
+给出数组A = [1,2,3] B = [4,5]，中位数 3
+
+挑战
+时间复杂度为O(log n)
+"""
+
+"""
+方法一：sort O(nlogn)
+"""
+class Solution:
+    """
+    @param: A: An integer array
+    @param: B: An integer array
+    @return: a double whose format is *.5 or *.0
+    """
+    def findMedianSortedArrays(self, A, B):
+        # write your code here
+        C = sorted(A + B)
+        n = len(C)
+        return C[n // 2] if n % 2 != 0 else (C[n // 2] + C[n // 2 - 1]) / 2
+
+
+"""
+方法二：merge只计数，不开辟新空间 O(n)
+"""
+class Solution:
+    """
+    @param: A: An integer array
+    @param: B: An integer array
+    @return: a double whose format is *.5 or *.0
+    """
+
+    def findMedianSortedArrays(self, A, B):
+        # write your code here
+        n = len(A) + len(B)
+        i = j = m1 = m2 = 0
+
+        while i + j <= n / 2:
+            if i < len(A) and j < len(B):
+                m2 = m1
+                if A[i] < B[j]:
+                    m1 = A[i]
+                    i += 1
+                else:
+                    m1 = B[j]
+                    j += 1
+            elif i < len(A):
+                m2 = m1
+                m1 = A[i]
+                i += 1
+            elif j < len(B):
+                m2 = m1
+                m1 = B[j]
+                j += 1
+
+        if n % 2 == 0:
+            return (m1 + m2) / 2.0
+        else:
+            return m1
+
+
+"""
+方法三：二分答案 O(logn)
+"""
+class Solution:
+    """
+    @param: A: An integer array
+    @param: B: An integer array
+    @return: a double whose format is *.5 or *.0
+    """
+
+    def findMedianSortedArrays(self, A, B):
+        # write your code here
+        m, n = len(A), len(B)
+        if m > n:
+            A, B, m, n = B, A, n, m
+
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and B[j - 1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i - 1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+                if i == 0:
+                    max_of_left = B[j - 1]
+                elif j == 0:
+                    max_of_left = A[i - 1]
+                else:
+                    max_of_left = max(A[i - 1], B[j - 1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2.0
---
+++ ch09/0486-merge-k-sorted-arrays.py
@@ -0,0 +1,94 @@
+"""
+486. Merge K Sorted Arrays
+将 k 个排序数组合并为一个大的排序数组。
+
+样例
+给出下面的 3 个排序数组：
+
+[
+  [1, 3, 5, 7],
+  [2, 4, 6],
+  [0, 8, 9, 10, 11]
+]
+合并后的大数组应为：
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+挑战
+在 O(N log k) 的时间复杂度内完成：
+
+N 是所有数组包含的整数个数。
+k 是数组的个数。
+"""
+
+"""
+方法一：利用堆，适合海量数据处理
+"""
+import heapq
+
+
+class Solution:
+    """
+    @param arrays: k sorted integer arrays
+    @return: a sorted array
+    """
+
+    def mergekSortedArrays(self, arrays):
+        # write your code here
+        res, heap = [], []
+
+        # 将每个数组中的第一个元素push进堆
+        for index, array in enumerate(arrays):
+            if len(array) == 0:
+                continue
+            heapq.heappush(heap, (array[0], array, 1))
+
+        # 每一轮pop一个最小值，并把同一数组的下一个值push入堆
+        while len(heap):
+            val, arr, next = heap[0]
+            heapq.heappop(heap)
+            res.append(val)
+            if next < len(arr):
+                heapq.heappush(heap, (arr[next], arr, next + 1))
+
+        return res
+
+"""
+方法二：两两归并
+"""
+class Solution:
+    """
+    @param arrays: k sorted integer arrays
+    @return: a sorted array
+    """
+
+    def mergekSortedArrays(self, arrays):
+        # write your code here
+        if not arrays or len(arrays) == 0:
+            return []
+        if len(arrays) == 1:
+            return arrays[0]
+
+        mid = len(arrays) // 2
+        left = self.mergekSortedArrays(arrays[mid:])
+        right = self.mergekSortedArrays(arrays[:mid])
+        return self.mergeSort(left, right)
+
+    def mergeSort(self, arr1, arr2):
+        res = []
+        i, j = 0, 0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                res.append(arr1[i])
+                i += 1
+            else:
+                res.append(arr2[j])
+                j += 1
+
+        while i < len(arr1):
+            res.append(arr1[i])
+            i += 1
+        while j < len(arr2):
+            res.append(arr2[j])
+            j += 1
+
+        return res
