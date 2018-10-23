483d12cc59999490daa5a64ee0e014e17d833846
zzy <zzy1120716@126.com>
2018-10-23 17:08:13 +0800

    ch01 #200

--- ch01/0200-longest-palindrome.py
+++ ch01/0200-longest-palindrome.py
@@ -10,6 +10,46 @@
 O(n2) 时间复杂度的算法是可以接受的，如果你能用 O(n)
 的算法那自然更好。
 """
+
+
+"""
+方法一：从中心扩张
+"""
+class Solution:
+
+    # lo is the index of palindromic string
+    # max_len is the length of palindromic string
+    # since while loop breaks after j -= 1, it requires lo index to start with j + 1
+    lo = max_len = 0
+
+    """
+    @param s: input string
+    @return: the longest palindromic substring
+    """
+    def longestPalindrome(self, s):
+        # write your code here
+        len_s = len(s)
+        if len_s < 2:
+            return s
+
+        for i in range(len_s - 1):
+            self.extendPalindrome(s, i, i)  # assume odd length, try to extend Palindrome as possible
+            self.extendPalindrome(s, i, i + 1)  # assume even length
+
+        return s[self.lo: self.lo + self.max_len]
+
+    def extendPalindrome(self, s, j, k):
+        while j >= 0 and k < len(s) and s[j] == s[k]:
+            j -= 1
+            k += 1
+        if self.max_len < k - j - 1:
+            self.lo = j + 1
+            self.max_len = k - j - 1
+
+
+"""
+方法二：动态规划
+"""
 class Solution:
     """
     @param s: input string
@@ -20,7 +60,7 @@ class Solution:
         # DP solution
         n = len(s)
         start, end = 0, 0
-        result = [[False] * n for i in range(n)]
+        result = [[False] * n for _ in range(n)]
         
         # Every 1-letter substring is a palindrome
         for i in range(n):
