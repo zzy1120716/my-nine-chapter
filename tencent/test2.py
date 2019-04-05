0eafe85c70b72fbf8ac850890f330669822f1cba
zzy <zzy1120716@126.com>
2019-04-05 22:54:33 +0800

    腾讯

--- tencent/test2.py
+++ tencent/test2.py
@@ -1,4 +1,35 @@
+"""
+示例：
+输入：
+3
+8 5 10
+1 1 2
+
+输出：
+2
+"""
+
+
 if __name__ == "__main__":
     n = int(input())
-    dl = list(map(int, input().split()))
-    pl = list(map(int, input().split()))
+    d = list(map(int, input().split()))
+    p = list(map(int, input().split()))
+
+    q = []
+
+    ans = now = 0
+    for i in range(n):
+        if now < d[i] and p[i] == 1:
+            ans += 1
+            now += d[i]
+        elif now >= d[i] and p[i] == 1:
+            q.append(d[i])
+        elif now < d[i] and p[i] == 2:
+            if not q or now + q[0] < d[i]:
+                ans += 2
+                now += d[i]
+            else:
+                ans += 1
+                now += q[0]
+                q.pop()
+    print(ans)
