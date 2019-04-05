6c3a6db17c637d365e04445752130bc37ca064e8
zzy <zzy1120716@126.com>
2019-04-05 21:57:04 +0800

    word search

--- ch07/related/0123-word-search.py
+++ ch07/related/0123-word-search.py
@@ -18,9 +18,42 @@ word = "SEE"，-> 返回 true,
 word = "ABCB"， -> 返回 false.
 """
 
+from collections import deque
+
+
+class Solution:
+    def exist(self, board, word) -> bool:
+        if not board:
+            return False
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.helper(board, i, j, word):
+                    return True
+
+        return False
+
+    def helper(self, board, i, j, word):
+        if len(word) == 0:
+            return True
+
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
+            return False
+
+        tmp = board[i][j]
+        board[i][j] = '#'
+
+        res = self.helper(board, i + 1, j, word[1:]) or self.helper(board, i - 1, j, word[1:]) \
+              or self.helper(board, i, j + 1, word[1:]) or self.helper(board, i, j - 1, word[1:])
+
+        # backtracking
+        board[i][j] = tmp
+
+        return res
+
 
 # DFS
-class Solution:
+class Solution1:
     """
     @param board: A list of lists of character
     @param word: A string
@@ -64,7 +97,7 @@ class Solution:
 
 
 # 简洁版本 DFS 记忆化搜索， 没有使用整个矩阵，只变化当前位置，判断后再置回
-class Solution1:
+class Solution2:
     """
     @param board: A list of lists of character
     @param word: A string
@@ -97,19 +130,62 @@ class Solution1:
         return res
 
 
+# BFS方法有bug，这道题不适合用BFS，需要记录每条路径中已访问的节点，非常耗费空间。
+class SolutionWrong:
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if word == '':
+            return True
+
+        m, n = len(board), len(board[0])
+        MOVES = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visited = set()
+        q = deque()
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    if len(word) == 1:
+                        return True
+                    q.append((i, j))
+                    index = 0
+                    visited.clear()
+                    while q:
+                        index += 1
+                        x, y = q.popleft()
+                        visited.add((x, y))
+                        for dx, dy in MOVES:
+                            nx = x + dx
+                            ny = y + dy
+                            if index < len(word) and 0 <= nx < m and 0 <= ny < n and (nx, ny) not in visited \
+                                    and board[nx][ny] == word[index]:
+                                if index == len(word) - 1:
+                                    return True
+                                visited.add((nx, ny))
+                                q.append((nx, ny))
+
+        return False
+
+
 if __name__ == '__main__':
-    print(Solution1().exist([
-        "ABCE",
-        "SFCS",
-        "ADEE"
-    ], "ABCCED"))
-    print(Solution1().exist([
-        "ABCE",
-        "SFCS",
-        "ADEE"
-    ], "SEE"))
-    print(Solution1().exist([
-        "ABCE",
-        "SFCS",
-        "ADEE"
-    ], "ABCB"))
+    print(Solution().exist([
+        ["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]
+    ], "ABCCED"))       # True
+    print(Solution().exist([
+        ["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]
+    ], "SEE"))          # True
+    print(Solution().exist([
+        ["A", "B", "C", "E"],
+        ["S", "F", "C", "S"],
+        ["A", "D", "E", "E"]
+    ], "ABCB"))         # False
+
+    print(Solution().exist([["C", "A", "A"], ["A", "A", "A"], ["B", "C", "D"]], "AAB"))        # True
